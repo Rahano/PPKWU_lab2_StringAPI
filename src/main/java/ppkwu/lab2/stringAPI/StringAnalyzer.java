0c4e8d4219fd0f5384458b41e4500ef872ace16a
Rahano <wojciechmuras@wp.pl>
2020-11-03 14:27:07 +0100

    Add input parameter for string analyzer

--- src/main/java/ppkwu/lab2/stringAPI/StringAnalyzer.java
+++ src/main/java/ppkwu/lab2/stringAPI/StringAnalyzer.java
@@ -1,13 +1,14 @@
 package ppkwu.lab2.stringAPI;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringAnalyzer {
 
     @RequestMapping(path = "/stringAnalyzer")
-    public String reverseString(){
+    public String reverseString(@RequestParam(value = "input", required = false) String input){
 
         return null;
 
