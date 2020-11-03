38cd0fd6bf5f7e57d7f470d61e6527bb7d68b1ad
Rahano <wojciechmuras@wp.pl>
2020-11-03 14:24:01 +0100

    Add mapping for string analyzer

--- src/main/java/ppkwu/lab2/stringAPI/StringAnalyzer.java
+++ src/main/java/ppkwu/lab2/stringAPI/StringAnalyzer.java
@@ -1,8 +1,15 @@
 package ppkwu.lab2.stringAPI;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringAnalyzer {
 
+    @RequestMapping(path = "/stringAnalyzer")
+    public String reverseString(){
+
+        return null;
+
+    }
 }
