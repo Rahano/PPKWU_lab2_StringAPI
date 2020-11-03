52662fec635c1b6503079d9ab745c2ff0452aaf5
Rahano <wojciechmuras@wp.pl>
2020-11-03 14:20:08 +0100

    Initialize project

---
+++ src/main/java/ppkwu/lab2/stringAPI/StringApiApplication.java
@@ -0,0 +1,13 @@
+package ppkwu.lab2.stringAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ppkwu/lab2/stringAPI/StringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ppkwu.lab2.stringAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
